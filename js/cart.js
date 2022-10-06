cb72275ceb2061490dfc25e0d4968a2337f5065d
flxx-ji <cinelli2180@gmail.com>
2022-10-06 20:09:52 +0200

    mise en place bouton quantite/formulaire

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,7 @@
 let ls = JSON.parse(localStorage.getItem('produit'));
 
 console.log(ls);
-
+   
 let itemCard = [];
 
 let productLs = [];
@@ -62,14 +62,10 @@ function sommeArticle () {
 }
 sommeArticle();
 
- 
- let btn_suppr = document.getElementsByClassName('deleteItem');
- console.log(btn_suppr);
+  
 
-
-
-function supprItem  (){
-      let btn_suppr = document.querySelectorAll('deleteItem');
+function supprItem(){
+      let btn_suppr = document.querySelectorAll('.deleteItem');
       console.log(btn_suppr);
     
     for (let a = 0; a < btn_suppr.length; a++) {
@@ -79,10 +75,198 @@ function supprItem  (){
       localStorage.setItem('produit', JSON.stringify(ls));
       alert("article supprime");
       location.reload();
-    })
+    });
      
   }
     //  btn_suppr.addEventListener(click,);
 }
  
  supprItem();
+
+//  function changeQty() {
+//   let btn_qty = document.querySelectorAll('.itemQuantity');
+//   console.log(btn_qty);
+//   for (let e = 0; 0 < btn_qty.length; e++){
+//     btn_qty[e].addEventListener('change',(event) => {
+//       let result = document.querySelectorA('.result');
+//       result.textContent = `la quantite de l'article est de ${event.target.value}`;
+
+//     }, )
+//   }
+//  }
+ // choix quantite et rechargement de pages
+ function changeQty2(){
+  let btn_qty = document.querySelectorAll('.itemQuantity');
+  for (let d = 0; d < btn_qty.length; d++){
+    btn_qty[d].addEventListener('change', function (event) {
+      let qtyModified = parseInt(btn_qty[d].value , 10);
+      console.log(qtyModified);
+      ls[d].quantity = qtyModified;
+      localStorage.setItem('produit', JSON.stringify(ls));
+      location.reload();
+    });
+  }
+   
+ }
+  changeQty2();
+
+
+           //------------formulaire------------//
+
+                
+
+document.querySelector('#order').addEventListener('click', function (e) {
+
+  let contactForm = {
+      firstName : document.querySelector('#firstName').value,
+      lastName : document.querySelector('#lastName').value,
+      address : document.querySelector('#address').value,
+      city : document.querySelector('#city').value,
+      email : document.querySelector('#email').value
+   };
+   
+  console.log(contactForm);
+
+//----------firstname----------//
+   function testFirstName() {
+     
+      let firstNameErrorMsg = document.getElementById('firstNameErrorMsg');
+
+     let firstNameRegExp = new RegExp(/^[A-Za-z][A-Za-z' -]*$/);
+      
+      if (firstNameRegExp.test(contactForm.firstName)) {
+       firstNameErrorMsg.textContent = '';
+       return true;
+     } else {
+       firstNameErrorMsg.textContent = 'votre nom est invalide';
+       return false;
+     }
+   }
+
+   testFirstName();
+
+
+//--------------Lastname--------------//
+
+
+   function testLastName() {
+     let lastNameErrorMsg = document.getElementById('lastNameErrorMsg');
+
+     let lastNameRegExp = new RegExp(/^[A-Za-z][A-Za-z' -]*$/);
+
+     if (lastNameRegExp.test(contactForm.lastName)) {
+       lastNameErrorMsg.textContent = '';
+       return true;
+     } else {
+       lastNameErrorMsg.textContent = 'votre nom est invalide';
+       return false;
+     }
+   }
+   testLastName();
+// ----------------Address--------------//
+   function testAddress() {
+     let addressErrorMsg = document.getElementById('addressErrorMsg');
+
+     let addressRegExp = new RegExp(/^[a-zA-Z0-9\s\,\''\-]*$/);
+
+     if (addressRegExp.test(contactForm.address)) {
+       addressErrorMsg.textContent = ``;
+       return true;
+     } else {
+       addressErrorMsg.textContent = `votre adresse est invalide`;
+       return false;
+     }
+   }
+
+   testAddress();
+
+//---------------city/ville----------------//
+   function testCity () {
+
+    let cityErrorMsg = document.getElementById('cityErrorMsg');
+
+    let cityRegExp = new RegExp (/^[A-Za-z]{3,20}$/);
+
+    if (cityRegExp.test(contactForm.city)){
+      cityErrorMsg.textContent = ``;
+      return true;
+    }else{
+      cityErrorMsg.textContent = `le nom de la ville est invalide`;
+      return false;
+    }
+   }
+
+   testCity();
+
+   //-------------Email-------------//
+
+   function testEmail() {
+
+    let emailErrorMsg = document.getElementById('emailErrorMsg');
+
+    let emailRegExp = new RegExp(/^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/);
+
+    if (emailRegExp.test(contactForm.email)){
+      emailErrorMsg.textContent = ``;
+      return true;
+    } else {
+      emailErrorMsg.textContent = `votre mail est invalide`;
+      return false;
+    }
+   }
+
+   testEmail();
+
+
+});
+
+
+
+
+
+      //---------lastName/nom de famille--------//
+    
+    
+       
+
+   //--------address/adresse---------
+
+   
+    
+
+    
+     //--------city/ville--------//
+      
+
+     //-----------Email--------//
+
+      
+ 
+
+
+     //--------BoutonCommander----------;;
+    //  let BoutonCommander = document.querySelector('#order');
+    //  BoutonCommander.addEventListener('click', function () {
+    //   validCommande(this);
+    //  });
+
+    //  const validCommande = function (inputSubmit){
+    //    let testOrder = OrderRegExp.test(inputSubmit.value);
+    //    console.log(testOrder);
+    //  };
+       
+    //  ----------tableau----------//
+
+    //  let tab = [
+    //    prenom = document.querySelector('#firstName'),
+
+    //    nom = document.querySelector('#lastName'),
+
+    //    adresse = document.querySelector('#address'),
+      
+    //    ville = document.querySelector('#city'),
+
+    //    email = document.querySelector('#email')
+    //  ];
+
+      
